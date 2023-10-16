50e9518a2c3c55c76c7becc51fd50dda1e62d014
rosol111 <gulamrosul037@gmail.com>
2023-10-17 02:18:13 +0600

    Added view all coffee and create coffee card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./Components/CoffeeCard";
 
 function App() {
+  const coffees = useLoaderData();
   return (
-    <>
-      <h1 className="text-6xl text-purple-500">Vite + React</h1>
-    </>
+    <div className="m-20 ">
+      <h1 className="text-6xl text-center text-purple-500">
+        Hot Hot Hot Cool Coffee:{coffees.length}
+      </h1>
+      <div className="grid md:grid-cols-2 gap-6 mt-8">
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
